c742949f62cf8966aad389dfb18c3b18f7fb5179
Hanru Vorster <hanru.vorster@gmail.com>
2025-02-05 11:11:24 +0200

    array type practice

--- index.ts
+++ index.ts
@@ -37,11 +37,13 @@ const you: {
   lastName: string;
   isReturning: boolean;
   age: number;
+  stayedAt: string[];
 } = {
   firstName: "Bobby",
   lastName: "Brown",
   isReturning: true,
   age: 35,
+  stayedAt: ["florida-home", "oman-flat", "tokyo-bungalow"],
 };
 
 function populateUser(isReturning: boolean, userName: string) {
