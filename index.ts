c8152ac30be878093613fcd061330d9d474c43f2
Hanru Vorster <hanru.vorster@gmail.com>
2025-02-05 11:50:41 +0200

    tuple type practice

--- index.ts
+++ index.ts
@@ -75,7 +75,7 @@ const properties: {
     code: number;
     country: string;
   };
-  contact: string;
+  contact: [number, string];
   isAvailable: boolean;
 }[] = [
   {
@@ -88,7 +88,7 @@ const properties: {
       code: 45632,
       country: "Colombuia",
     },
-    contact: "marywinkle@gmail.com",
+    contact: [+1123495082908, "marywinkle@gmail.com"],
     isAvailable: true,
   },
   {
@@ -101,7 +101,7 @@ const properties: {
       code: 343903,
       country: "Poland",
     },
-    contact: "garydavis@hotmail.com",
+    contact: [+1123495082908, "garydavis@hotmail.com"],
     isAvailable: false,
   },
   {
@@ -114,7 +114,7 @@ const properties: {
       code: 35433,
       country: "United Kingdom",
     },
-    contact: "andyluger@aol.com",
+    contact: [+1123495082908, "andyluger@aol.com"],
     isAvailable: true,
   },
 ];
